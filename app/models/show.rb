543590ec78ea6a7734f01c514c041794d6d6550d
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-03 18:42:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,10 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
+
+  def self.most_popular_show
+    Show.highest_rating
 
 end
